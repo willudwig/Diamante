c05a30a47da4ddd064428a39b78b28ca772ec23e
William Ludwig <acesso.william@gmail.com>
2022-02-21 16:47:45 -0300

    Estruturado

--- Diamante.ConsoleApp/Program.cs
+++ Diamante.ConsoleApp/Program.cs
@@ -7,78 +7,30 @@ namespace Diamante.ConsoleApp
         static void Main(string[] args)
         {
             int num;
+            int meio;
 
-            while (true)
-            {
-                Console.Write("Digite um número ímpar: ");
-                num = int.Parse(Console.ReadLine());
+            InputUsuario(out num, out meio);
 
-                if (num % 2 != 0)
-                {
-                    break;
-                }
-                else if (num % 2 == 0)
-                {
-                    continue;
-                }
-            }
-            Console.WriteLine();
-
-            int meio = num / 2;
-            
             string[] vetor = new string[num];
-            
-            //inicia vetor
-            for (int i = 0; i < vetor.Length; i++)
-            {
-                vetor[i] = " ";
-            }
 
-            vetor[meio] = "x";
+            IniciarVetor(meio, vetor);
+            AcresecerDiamante(meio, vetor);
+            DecrescerDiamante(num, meio, vetor);
 
-            for (int i = 0; i < vetor.Length; i++)
-            {
-                Console.Write(vetor[i]);
-            }
-            Console.WriteLine();
+            Console.ReadKey();
+        }
 
-            //aumenta o diamante
-            for (int i = 1; i <= vetor.Length; i++)
-            {
-                if (i >= 0)
-                {
-                    if (vetor[0]  == " ")
-                    {
-                        vetor[meio] = "x";
-                        vetor[meio + i] = "x";
-                        vetor[meio - i] = "x";
-                    }
-                    else if (vetor [0] == "x")
-                    {
-                        break;
-                    }
-                }
-                else
-                    break;
-
-                    //exibe
-                    for (int j = 0; j < vetor.Length; j++)
-                {
-                    Console.Write(vetor[j]);
-                }
-                Console.WriteLine();
-            }
-
-            //diminui o diamante
+        private static void DecrescerDiamante(int num, int meio, string[] vetor)
+        {
             for (int i = 1; i <= vetor.Length; i++)
             {
                 if (i >= 0)
                 {
                     if (vetor[meio - 1] == "x")
                     {
-                        vetor[ meio ] = "x";
-                        vetor[ num - i ] = " ";
-                        vetor[ i - 1 ] = " ";
+                        vetor[meio] = "x";
+                        vetor[num - i] = " ";
+                        vetor[i - 1] = " ";
                     }
                     else if (vetor[meio - 1] == " ")
                     {
@@ -95,7 +47,72 @@ namespace Diamante.ConsoleApp
                 }
                 Console.WriteLine();
             }
-            Console.ReadKey();
+        }
+
+        private static void AcresecerDiamante(int meio, string[] vetor)
+        {
+            for (int i = 1; i <= vetor.Length; i++)
+            {
+                if (i >= 0)
+                {
+                    if (vetor[0] == " ")
+                    {
+                        vetor[meio] = "x";
+                        vetor[meio + i] = "x";
+                        vetor[meio - i] = "x";
+                    }
+                    else if (vetor[0] == "x")
+                    {
+                        break;
+                    }
+                }
+                else
+                    break;
+
+                //exibe
+                for (int j = 0; j < vetor.Length; j++)
+                {
+                    Console.Write(vetor[j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void IniciarVetor(int meio, string[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = " ";
+            }
+
+            vetor[meio] = "x";
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i]);
+            }
+            Console.WriteLine();
+        }
+
+        private static void InputUsuario(out int num, out int meio)
+        {
+            while (true)
+            {
+                Console.Write("Digite um número ímpar: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 != 0)
+                {
+                    break;
+                }
+                else if (num % 2 == 0)
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine();
+
+            meio = num / 2;
         }
     }
 }
